635ca2058817265d453f4334e7382e58d080df5e
Chris-AM <c.aranguizm@outlook.com>
2021-01-07 16:33:54 -0300

    wildcard added

--- electron-quick-start/ngElectron/src/app/app-routing.module.ts
+++ electron-quick-start/ngElectron/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactInfoComponent } from './components/contact-info/contact-info.component';
 import { ProductsComponent } from './components/products/products.component';
 import { SellsComponent } from './components/sells/sells.component';
 
 const routes: Routes = [
   { path: 'sells', component: SellsComponent },
-  { path: 'products', component: ProductsComponent }
+  { path: 'products', component: ProductsComponent },
+  { path: 'contactInfo', component: ContactInfoComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'sells' }
 ];
 
 @NgModule({
