9a8dfe55f4214193d1fcfea0397536bc48c6e885
Chris-AM <c.aranguizm@outlook.com>
2021-01-16 00:16:57 -0300

    indexing routes, cors and morgan

--- electron-quick-start/dataBase/server/build/index.js
+++ electron-quick-start/dataBase/server/build/index.js
@@ -4,16 +4,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const indexroutes_1 = __importDefault(require("./routes/indexroutes"));
+const sqlRoutes_1 = __importDefault(require("./routes/sqlRoutes"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
 class Server {
     constructor() {
+        //here I execute al these methods
         this.app = express_1.default();
         this.config();
         this.routes();
     }
     config() {
         this.app.set('port', process.env.PORT || 3000);
+        //Morgan is a HTTP request logger middleware for Node. js. It simplifies the process of logging requests to your application.
+        //now I can see the petitions to the server
+        this.app.use(morgan_1.default('dev'));
+        this.app.use(cors_1.default());
+        this.app.use(express_1.default.json());
+        //urlencoded: parses incoming requests with urlencoded payloads and is based on body-parser.
+        this.app.use(express_1.default.urlencoded({ extended: false }));
     }
     routes() {
+        this.app.use('/', indexroutes_1.default);
+        this.app.use('/api/store', sqlRoutes_1.default);
     }
     start() {
         this.app.listen(this.app.get('port'), () => {
--- electron-quick-start/dataBase/server/src/index.ts
+++ electron-quick-start/dataBase/server/src/index.ts
@@ -1,26 +1,40 @@
 import expres, { Application } from 'express';
-
-class Server{
+import indexRoutes from './routes/indexroutes';
+import sqlRoutes from './routes/sqlRoutes';
+import morgan from 'morgan';
+import cors from 'cors';
+import e from 'cors';
+class Server {
 
   public app: Application;
 
   constructor() {
+    //here I execute al these methods
+
     this.app = expres();
     this.config();
     this.routes();
-}
+  }
 
   config(): void {
     this.app.set('port', process.env.PORT || 3000);
+    //Morgan is a HTTP request logger middleware for Node. js. It simplifies the process of logging requests to your application.
+    //now I can see the petitions to the server
+    this.app.use(morgan('dev'));
+    this.app.use(cors());
+    this.app.use(expres.json());
+    //urlencoded: parses incoming requests with urlencoded payloads and is based on body-parser.
+    this.app.use(expres.urlencoded({extended: false}))
   }
 
-  routes(): void{
-
+  routes(): void {
+    this.app.use('/', indexRoutes);
+    this.app.use('/api/store', sqlRoutes);
   }
 
-  start(): void{
+  start(): void {
     this.app.listen(this.app.get('port'), () => {
-      console.log(`server on port` , this.app.get('port'))
+      console.log(`server on port`, this.app.get('port'))
     });
   }
 
