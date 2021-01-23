fcdf6e7bc5d4ad96a3deb04e34c66a665839c394
Chris-AM <c.aranguizm@outlook.com>
2021-01-22 23:32:00 -0300

    importing databse, exporting class

--- electron-quick-start/API/server/build/controllers/sellsPointController.js
+++ electron-quick-start/API/server/build/controllers/sellsPointController.js
@@ -1,11 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.SellsPointController = void 0;
+const database_1 = __importDefault(require("../database"));
 class SellsPointController {
     constructor() {
     }
     index(req, res) {
-        res.send('testing');
+        database_1.default.query('DESCRIBE producto');
+        res.json('SellsPoint');
     }
 }
-exports.SellsPointController = SellsPointController;
+const sellsPointController = new SellsPointController();
+exports.default = sellsPointController;
