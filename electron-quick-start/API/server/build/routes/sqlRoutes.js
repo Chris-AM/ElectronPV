4ae1cf3461f09395d60b19a4883d43ac1f6dac95
Chris-AM <c.aranguizm@outlook.com>
2021-01-22 23:33:49 -0300

    importing and geting SellsPointController

--- electron-quick-start/API/server/build/routes/sqlRoutes.js
+++ electron-quick-start/API/server/build/routes/sqlRoutes.js
@@ -1,6 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const sellsPointController_1 = __importDefault(require("../controllers/sellsPointController"));
 class SqlRoutes {
     constructor() {
         this.router = express_1.Router();
@@ -10,7 +14,7 @@ class SqlRoutes {
         //getMethod(expects 1 pameter at least)
         //send(property) Response<any, number>.send: Send<any, Response<any, number>>
         /*what i'm doing */
-        this.router.get('/', (req, res) => res.send('electron PV'));
+        this.router.get('/', sellsPointController_1.default.index);
     }
 }
 const sqlRoutes = new SqlRoutes();
--- electron-quick-start/API/server/src/routes/sqlRoutes.ts
+++ electron-quick-start/API/server/src/routes/sqlRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import sellsPointController from '../controllers/sellsPointController'
 class SqlRoutes{
 
   public router: Router = Router();
@@ -12,9 +12,8 @@ class SqlRoutes{
     //getMethod(expects 1 pameter at least)
     //send(property) Response<any, number>.send: Send<any, Response<any, number>>
     /*what i'm doing */
-    this.router.get('/', (req, res) => res.send(
-      'electron PV'
-    ))
+    this.router.get('/', sellsPointController.index
+    )
   }
 }
 
