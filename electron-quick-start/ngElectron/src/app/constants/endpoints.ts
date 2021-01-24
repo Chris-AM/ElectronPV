f956e131c1a4e210bdb6db25562fb925554e977e
Chris-AM <c.aranguizm@outlook.com>
2021-01-24 16:58:23 -0300

    constants folder moved and endpoints.ts was added

---
+++ electron-quick-start/ngElectron/src/app/constants/endpoints.ts
@@ -0,0 +1,11 @@
+export const ENDPOINTS = {
+  SERVICES: {
+    BASE_URL: 'http://localhost:3000/api/',
+    PRODUCTS: 'product',
+    CLIENTS: 'client',
+    STORE: 'store',
+    CLOSES: 'close',
+    SELLS: 'sell',
+    TICKETS: 'ticket'
+  }
+}
