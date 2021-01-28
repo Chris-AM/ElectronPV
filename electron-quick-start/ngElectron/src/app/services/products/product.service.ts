314591dc5874494c79cd06435c79a1b9c43ad2b7
Chris-AM <c.aranguizm@outlook.com>
2021-01-28 08:03:08 -0300

    adding products logic

--- electron-quick-start/ngElectron/src/app/services/products/product.service.ts
+++ electron-quick-start/ngElectron/src/app/services/products/product.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import _ from 'lodash';
+import { ENDPOINTS } from 'src/app/constants/endpoints';
+import { Product } from "../../models/product";
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getQuery(query: string) {
+    const url = _.get(ENDPOINTS, 'SERVICES.BASE_URL') + `${query}`;
+    return this.httpClient.get(url);
+  }
+
+  getProducts() {
+    let products = _.get(ENDPOINTS, 'SERVICES.PRODUCTS');
+    return this.getQuery(products);
+  }
+
+  getProduct(codigo_barras: string) {
+    let product = _.get(ENDPOINTS, 'SERVICES.PRODUCTS') + `${codigo_barras}`;
+    return this.getQuery(product);
+  }
+
+  saveProduct(product: Product) {
+    let urlProduct = _.get(ENDPOINTS, 'SERVICES.BASE_URL') + _.get(ENDPOINTS, 'SERVICES.PRODUCTS') ;
+    return this.httpClient.post(urlProduct, product)
+  }
+
+  updateProduct(codigo_barras: string | number, updatedProduct: Product): Observable<any> {
+    let urlProduct = _.get(ENDPOINTS, 'SERVICES.BASE_URL') + _.get(ENDPOINTS, 'SERVICES.PRODUCTS') + `${codigo_barras}`;
+    return this.httpClient.put(urlProduct, updatedProduct);
+  }
+
+  deleteProduct(codigo_barras: string) {
+    let urlProduct = _.get(ENDPOINTS, 'SERVICES.BASE_URL') + _.get(ENDPOINTS, 'SERVICES.PRODUCTS') + `${codigo_barras}`;
+    return this.httpClient.delete(urlProduct)
+  }
 }
